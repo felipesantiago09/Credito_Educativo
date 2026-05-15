b6e9b7c5708b7e7028b984359da218da91b6cb47
Esteban Muneton <estebanmuneton58@gmail.com>
2026-05-15 17:12:01 -0500

    intento de eliminacion env

--- src/model/connection.py
+++ src/model/connection.py
@@ -1,19 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
 
-# Cargar variables del .env
-load_dotenv()
+DATABASE_URL = "postgresql://adminesteban_y_felipe:20uOvMkWmwxQyKgUwckPmGmvggeEkH1f@dpg-d81jsecvikkc73a5rgjg-a.oregon-postgres.render.com:5432/credito_db"
 
-# Leer DATABASE_URL
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Conexion PostgreSQL
 engine = create_engine(DATABASE_URL)
 
-# Sesion ORM
 Session = sessionmaker(bind=engine)
 
-# Base para modelos
 Base = declarative_base()
